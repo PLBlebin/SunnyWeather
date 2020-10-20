12084b10ee95ff80330d4ede9c2c6a3db25cf76e
Lebin <Lebin@gmail.com>
2020-10-20 23:17:14 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/non/sunnyweather/logic/dao/PlaceDao.kt
@@ -0,0 +1,26 @@
+package com.non.sunnyweather.logic.dao
+
+import android.content.Context
+import androidx.core.content.edit
+import com.google.gson.Gson
+import com.non.sunnyweather.SunnyWeatherApplication
+import com.non.sunnyweather.logic.model.Place
+
+object PlaceDao {
+
+    fun savePlace(place: Place) {
+        sharePreferences().edit {
+            putString("place", Gson().toJson(place))
+        }
+    }
+
+    fun getSavePlace(): Place {
+        val placeJson = sharePreferences().getString("place", "")
+        return Gson().fromJson(placeJson, Place::class.java)
+    }
+
+    fun isPlaceSaved() = sharePreferences().contains("place")
+
+    private fun sharePreferences() = SunnyWeatherApplication.context.
+        getSharedPreferences("sunny_weather", Context.MODE_PRIVATE)
+}
